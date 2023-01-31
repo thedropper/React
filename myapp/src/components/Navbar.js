0176cf6994e64b2b6f5af7d42bf68d5c342c1785
thedropper <rishabagrahari248@gmail.com>
2023-01-31 18:23:36 +0530

    changes in navbar

--- myapp/src/components/Navbar.js
+++ myapp/src/components/Navbar.js
@@ -15,7 +15,7 @@ export default function Navbar(props) {
       <nav className={`navbar navbar-expand-lg navbar-${props.mode} bg-${props.mode === "dark" ? "black":"#DAE84A"}`}>
             <div className="container-fluid">   
               <Link className="navbar-brand" to="Textplace">{props.title}</Link>
-              <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="/navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+              <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="true" aria-label="Toggle navigation">
                 <span className="navbar-toggler-icon"></span>
               </button>
               <div className="collapse navbar-collapse" id="navbarSupportedContent">
